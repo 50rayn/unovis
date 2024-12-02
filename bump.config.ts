fd80df5588a60109bdbf212609ae61bb80163bbe
Soryn <soryngitlan@gmail.com>
2024-12-02 23:57:57 +0200

    Release: 1.4.5

--- bump.config.ts
+++ bump.config.ts
@@ -3,10 +3,13 @@ import { defineConfig } from 'bumpp'
 export default defineConfig({
   commit: 'Release: %s',
   all: true,
-  files: [
-    'package.json',
-    'package-lock.json',
-    'packages/*/package.json',
-    'packages/*/package-lock.json',
-  ],
+  printCommits: false,
+  ignoreScripts: true,
+  recursive: true,
+  // files: [
+  //   'package.json',
+  //   // 'package-lock.json',
+  //   // 'packages/*/package.json',
+  //   // 'packages/*/package-lock.json',
+  // ],
 })
